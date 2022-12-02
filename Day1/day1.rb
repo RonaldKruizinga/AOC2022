e66d5716d17b00f3d983a57f50f59f79861b9060
Ronald Kruizinga <Ronald.M.Kruizinga@outlook.com>
2022-12-02 09:45:21 +0100

    day 1 part 2

--- Day1/day1.rb
+++ Day1/day1.rb
@@ -1,1 +1,4 @@
-puts File.readlines('Day1/input.txt', "\n\n").map { |s| s.rstrip.split("\n").map(&:to_i) }.map(&:sum).max
+list_of_sums = File.readlines('Day1/input.txt', "\n\n").map { |s| s.rstrip.split("\n").map(&:to_i) }.map(&:sum)
+puts list_of_sums.max
+
+puts list_of_sums.sort!.reverse!.take(3).sum
